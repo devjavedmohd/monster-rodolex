b27371853b03c646eb3483be82bb87f198fd1038
devjavedmohd <dev.javedmohd@gmail.com>
2023-01-08 22:37:37 +0530

    readme update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
+import {Component} from 'react';
+
 import logo from './logo.svg';
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            monsters: [],
+            searchField: ''
+            // name: {
+            //     firstName: 'Javed',
+            //     lastName: 'Mohd'
+            // },
+            // company: 'FBJ'
+        }
+    }
+    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then(
+                (user) => this.setState(() => {
+                    return { monsters: user }
+                })
+            )
+    }
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(() => {
+            return { searchField };
+        });
+    };
+  render() {
+      const {monsters, searchField} = this.state;
+      const {onSearchChange} = this;
+      
+      const filteredMonster = monsters.filter((monster) => {
+          return monster.name.toLocaleLowerCase().includes(searchField);
+      });
+      
+      return (
+        <div className="App">
+            <h1>Monster Rodolex</h1>
+            <SearchBox 
+                searchChanges={onSearchChange} 
+                placeholder='search monsters' 
+                className='search-box' />
+            <CardList monsters={filteredMonster} />
+        </div>
+      )
+    // return (
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="log o" />
+    //           <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I worked in {this.state.company}</p>
+    //           <button onClick={() => {
+    //               this.setState(() => {
+    //                   return {
+    //                       name: {
+    //                           firstName: 'Faham',
+    //                           lastName: 'Javed'
+    //                       }
+    //                   }
+    //               }, () => {
+    //                   console.log(this.state) // not mandatory
+    //               })
+    //           }}>Change Name</button>
+    //       </header>
+    //     </div>
+    // );  
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+﻿import { Component } from 'react';
+import './search-box.styles.css';
+class SearchBox extends Component {
+    render() {
+        const { searchChanges, placeholder, className } = this.props;
+        return (
+            <input type="search"
+                   className={ className }
+                   placeholder={ placeholder }
+                   onChange={ searchChanges }
+            />
+        )
+    }
+}
+
+export default SearchBox;
